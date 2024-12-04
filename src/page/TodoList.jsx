c4715c769199365b14bcd8da7f98644c98708c42
NhOuOiB <s33165309000@gmail.com>
2024-12-04 23:36:57 +0800

    [feat] add Todo List page

---
+++ src/page/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="">
+        <div className="text-3xl">Todo List</div>
+        <div className='text-gray-400'>Add things to do</div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
