6ca00911247315198e6aac446a837bcbd3da0770
NhOuOiB <s33165309000@gmail.com>
2024-12-07 18:30:59 +0800

    [feat] update App component styles with gradient background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoList from './page/TodoList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full h-screen bg-gradient-to-b from-[#eef7fe] to-[#ecedff] text-[#8795bd]">
       <TodoList />
     </div>
   );
