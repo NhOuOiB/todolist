b129b11baf88a7dcea187ece11b172f7c44839d4
NhOuOiB <s33165309000@gmail.com>
2024-12-07 18:31:27 +0800

    [feat] add AddToList component for add todo

---
+++ src/component/AddToList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+
+const AddToList = ({ todos, setTodos, shouldScrollRef }) => {
+  const [text, setText] = React.useState('');
+  const handleAdd = () => {
+    if (text.trim() === '') return;
+    const newTodos = [...todos, { text, done: false, time: new Date().toISOString() }];
+    setTodos(newTodos);
+    setText('');
+    shouldScrollRef.current = true;
+  };
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="mb-1">Add to list</div>
+      <div className="h-12 flex items-center gap-1">
+        <input
+          type="text"
+          className="w-full h-full rounded border px-2"
+          value={text}
+          onInput={(e) => handleInput(e)}
+          onKeyDown={(e) => e.key === 'Enter' && handleAdd()}
+        />
+        <div
+          className="w-16 h-full flex justify-center items-center bg-[#7890cb] hover:bg-[#5c7ccf] rounded border cursor-pointer"
+          onClick={handleAdd}
+        >
+          <IconContext.Provider value={{ className: 'text-white text-xl' }}>
+            <AiOutlinePlus />
+          </IconContext.Provider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddToList;
